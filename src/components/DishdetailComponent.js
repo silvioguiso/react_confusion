c11d310e4d8bbced76c8c7e1746500fccf54ddc0
Silvio Guiso <silvio.guiso@gmail.com>
2020-02-28 12:08:57 -0800

    Added main component and re-organized applicated

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -25,7 +25,7 @@ class DishDetail extends Component{
             return (
                 <li key={comment.id}>
                     <p>{comment.comment}</p>
-                    <p>-- {comment.author}, {comment.date}</p>
+                    <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
                 </li>
             )
         })
@@ -41,19 +41,21 @@ class DishDetail extends Component{
 
     render(){
 
-        const dish = this.props.selectedDish;
+        const dish = this.props.dish;
 
         if(dish == null){
             return <div></div>
         }        
 
         return (
-            <div className='row'>
-                <div className='col-12 col-md-5 m-1'>
-                    {this.renderDish(dish)}
-                </div>
-                <div className='col-12 col-md-5 m-1'>
-                    {this.renderComments(dish.comments)}
+            <div class="container">
+                <div className='row'>
+                    <div className='col-12 col-md-5 m-1'>
+                        {this.renderDish(dish)}
+                    </div>
+                    <div className='col-12 col-md-5 m-1'>
+                        {this.renderComments(dish.comments)}
+                    </div>
                 </div>
             </div>
         );

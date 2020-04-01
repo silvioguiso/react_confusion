7f3e14639c82c3f10fa28f325de4a20097108b12
Silvio Guiso <silvio.guiso@gmail.com>
2020-03-31 17:22:10 -0700

    Add redux thunk and logger.

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,6 +2,7 @@ import React, {Component } from 'react'
 import { Card , CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Modal, ModalHeader, ModalBody, Button, Row, Label } from 'reactstrap';
 import { Link } from 'react-router-dom'
 import { LocalForm, Control, Errors } from 'react-redux-form'
+import { Loading } from './LoadingComponent'
 
 const maxLength = (len) => (val) => !(val) || val.length <= len;
 const minLength = (len) => (val) => (val) && val.length >= len;
@@ -142,38 +143,52 @@ function RenderComments({comments, addComment, dishId}){
 
 const DishDetail = (props) => {
 
-    const dish = props.dish;
-
-    if(dish == null){
-        return <div></div>
+    if(props.isLoading){
+        return (
+            <div className='container'>
+                <div className='row'>
+                    <Loading />
+                </div>
+            </div>
+        )
     }
-
-    return (
-        <div className="container">
-            <div className='row'>
-                <Breadcrumb>
-                    <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
-                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-                </Breadcrumb>
-                <div className='col-12'>
-                    <h3>{props.dish.name}</h3>
-                    <hr />
+    else if(props.errMsg){
+        return (
+            <div className='container'>
+                <div className='row'>
+                    <h3>{props.errMsg}</h3>
                 </div>
             </div>
-            <div className='row'>
-                <div className='col-12 col-md-5 m-1'>
-                    <RenderDish dish={props.dish} />
+        )
+    }
+    else{
+
+        return (
+            <div className="container">
+                <div className='row'>
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className='col-12'>
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>
                 </div>
-                <div className='col-12 col-md-5 m-1'>
-                    <RenderComments 
-                        comments={props.comments}
-                        addComment={props.addComment}
-                        dishId={props.dish.id} />
+                <div className='row'>
+                    <div className='col-12 col-md-5 m-1'>
+                        <RenderDish dish={props.dish} />
+                    </div>
+                    <div className='col-12 col-md-5 m-1'>
+                        <RenderComments 
+                            comments={props.comments}
+                            addComment={props.addComment}
+                            dishId={props.dish.id} />
+                    </div>
                 </div>
             </div>
-        </div>
-    );
-
+        );
+    }
 }
 
 export default DishDetail;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -12,7 +12,7 @@ import About from './AboutComponent'
 //Libraries
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
-import { addComment } from '../redux/ActionCreators'
+import { addComment, fetchDishes } from '../redux/ActionCreators'
 
 
 const mapStateToProps = (state) => {
@@ -25,17 +25,24 @@ const mapStateToProps = (state) => {
 }
 
 const mapDispatchToProps = (dispatch) => ({
-    addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment))
+    addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
+    fetchDishes: () => {dispatch(fetchDishes())}
 })
 
 class Main extends Component{
+
+    componentDidMount(){
+        this.props.fetchDishes();
+    }
 
     render() {
 
         const HomePage = () => {
             return (
                 <Home 
-                    dish = {this.props.dishes.filter((dish) => dish.featured)[0]}
+                    dish = {this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+                    dishesLoading = {this.props.dishes.isLoading}
+                    dishesErrMsg = {this.props.dishes.errMsg}
                     leader = {this.props.leaders.filter((leader) => leader.featured)[0]}
                     promotion = {this.props.promotions.filter((promotion) => promotion.featured)[0]}
                 />
@@ -45,7 +52,9 @@ class Main extends Component{
         const DishWIthId = ({match}) => {
             return (
                 <DishDetail 
-                    dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+                    dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+                    isLoading = {this.props.dishes.isLoading}
+                    errMsg = {this.props.dishes.errMsg}
                     comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
                     addComment={this.props.addComment}
                 /> //10 is a base 10 integer
